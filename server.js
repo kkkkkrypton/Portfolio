7e6c623d069ffdbd188c7d4b41936df82768a62c
heather <heatherwanggg@gmail.com>
2023-10-04 01:01:00 -0400

    change port

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
 
-let port = process.env.PORT || 5000;
-app.listen(port);
+const port = process.env.PORT || 5000;
+const host = '0.0.0.0';
+app.listen(port, host, function () {
+    console.log('Server running at http://localhost:5000/');
+});
 module.exports = app;
 
-console.log('Server running at http://localhost:5000/');
+// console.log('Server running at http://localhost:5000/');
