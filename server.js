ecebcdb76f13a0fc52a1820025dea02046d53df6
heather <heatherwanggg@gmail.com>
2023-10-04 01:27:00 -0400

    start server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var express = require('./config/express');
 var app = express();
 
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port, "0.0.0.0", function () {
+    console.log('Server running at http://localhost:3000/');
+});
 
 module.exports = app;
 
-console.log('Server running at http://localhost:3000/');
+
